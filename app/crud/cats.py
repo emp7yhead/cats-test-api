55d64a40124d46154b93f61ae1ca8cd1272a60b8
emp7yhead <artyomkropp@gmail.com>
2022-07-27 23:42:18 +0900

    Add: get_cats in crud

---
+++ app/crud/cats.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.cats import Cats
+from app.schemas.cats import CatSchema
+
+
+async def get_cats(
+    session: AsyncSession,
+    offset: int,
+    limit: int,
+) -> List[CatSchema]:
+    all_cats = await session.execute(
+        select(Cats).limit(limit).offset(offset),
+    )
+    return all_cats.scalars().all()
