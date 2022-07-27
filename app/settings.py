6d93f07fbfff3df36ae3dd7e392ed2edc8f78197
emp7yhead <artyomkropp@gmail.com>
2022-07-27 23:44:00 +0900

    Update: settings

--- app/settings.py
+++ app/settings.py
@@ -1,11 +1,38 @@
-from pydantic import BaseSettings
+from typing import Any, Dict, Optional
 
+import dotenv
+from pydantic import BaseSettings, PostgresDsn, validator
+
+dotenv.load_dotenv('.env')
 
 class Settings(BaseSettings):
     """Config app."""
 
     APP_NAME: str = 'CatsAPI'
     API_VERSION: str = '/v1'
+    POSTGRES_SERVER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(
+        cls, v: Optional[str],
+        values: Dict[str, Any],
+    ) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
+    class Config:
+        case_sensitive = True
 
 
 settings = Settings()
