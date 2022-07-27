e12f7d11fb613526e2bf75bc6baa23ef5a326eaf
emp7yhead <artyomkropp@gmail.com>
2022-07-27 23:41:59 +0900

    Add: cats model

---
+++ app/models/cats.py
@@ -0,0 +1,29 @@
+"""Model for cat."""
+from sqlalchemy import Column, Enum, Integer
+
+from app.db.base_class import Base
+
+cat_color = Enum(
+    'black',
+    'white',
+    'black & white',
+    'red',
+    'red & black & white',
+    'red & white',
+    name='catcolor',
+)
+
+
+class Cats(Base):
+    """Define answer model."""
+
+    __tablename__ = 'cats'
+
+    name = Column(Integer, primary_key=True)
+    color = Column(cat_color)
+    tail_length = Column(Integer)
+    whiskers_length = Column(Integer)
+
+    def __repr__(self) -> str:
+        """Cat representation."""
+        return f'{self.name}'
