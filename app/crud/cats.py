2e55a229202bc18cf00aed10300829f97d37d858
emp7yhead <artyomkropp@gmail.com>
2022-07-27 23:49:44 +0900

    Fix: linter errors

--- app/crud/cats.py
+++ app/crud/cats.py
@@ -12,6 +12,7 @@ async def get_cats(
     offset: int,
     limit: int,
 ) -> List[CatSchema]:
+    """Get cats with/or offset and limit."""
     all_cats = await session.execute(
         select(Cats).limit(limit).offset(offset),
     )
