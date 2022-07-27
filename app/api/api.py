140c961fa5e459f65e212bf933f0a2ce3a282b83
emp7yhead <artyomkropp@gmail.com>
2022-07-27 23:42:53 +0900

    Update: api router

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import ping
+from app.api.endpoints import cats, ping
 
 api_router = APIRouter()
 api_router.include_router(ping.router, prefix='/ping', tags=['ping'])
+api_router.include_router(cats.router, prefix='/cats', tags=['cats'])
