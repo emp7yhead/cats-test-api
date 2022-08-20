a52763d87e56913fb7c7cedaacfa1e419c361415
emp7yhead <artyomkropp@gmail.com>
2022-08-20 22:00:38 +0900

    Add: post route

--- app/api/endpoints/cats.py
+++ app/api/endpoints/cats.py
@@ -1,9 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, status
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud.cats import get_cats
+from app.crud.cats import get_cats, post_cat
 from app.db.session import get_session
 from app.schemas.cats import CatSchema
 
@@ -15,7 +16,7 @@ async def get_all_cats(
     session: AsyncSession = Depends(get_session),
     offset: int | None = Query(default=None),
     limit: int | None = Query(default=None),
-):
+) -> List[CatSchema]:
     """Get list of all cats."""
     cats = await get_cats(session, offset, limit)
     return [
@@ -26,3 +27,27 @@ async def get_all_cats(
             whiskers_length=cat.whiskers_length,
         ) for cat in cats
     ]
+
+
+@router.post('/')
+async def create_cat(
+    cat: CatSchema,
+    session: AsyncSession = Depends(get_session),
+) -> CatSchema:
+    """Post a cat."""
+    new_cat = post_cat(
+        session=session,
+        name=cat.name,
+        color=cat.color,
+        tail_length=cat.tail_length,
+        whiskers_length=cat.whiskers_length,
+    )
+    try:  # noqa: WPS229
+        await session.commit()
+        return new_cat
+    except IntegrityError:
+        await session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Cat already exists!',
+        )
