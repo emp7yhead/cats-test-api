e82bd2ab99da9d4a5af345cd61a681efed968064
emp7yhead <artyomkropp@gmail.com>
2022-08-21 10:30:23 +0900

    Update: settings

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,7 @@
 from typing import Any, Dict, Optional
 
-import dotenv
 from pydantic import BaseSettings, PostgresDsn, validator
 
-dotenv.load_dotenv('.env')
 
 class Settings(BaseSettings):
     """Config app."""
@@ -16,7 +14,7 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
 
-    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    @validator('SQLALCHEMY_DATABASE_URI', pre=True)
     def assemble_db_connection(
         cls, v: Optional[str],
         values: Dict[str, Any],
@@ -24,15 +22,16 @@ class Settings(BaseSettings):
         if isinstance(v, str):
             return v
         return PostgresDsn.build(
-            scheme="postgresql+asyncpg",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
+            scheme='postgresql+asyncpg',
+            user=values.get('POSTGRES_USER'),
+            password=values.get('POSTGRES_PASSWORD'),
+            host=values.get('POSTGRES_SERVER'),
             path=f"/{values.get('POSTGRES_DB') or ''}",
         )
 
     class Config:
         case_sensitive = True
+        env_file = '.env'
 
 
 settings = Settings()
